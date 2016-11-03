7fb59925e5f9737c6b1bc0c4146719afee3041eb
thomasimarshall <marshallfortysix@gmail.com>
2016-11-03 23:46:01 +0000

    Added assignment functionality

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict'
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+function LunchCheckController ($scope) {
+
+  $scope.menu == ""
+
+  $scope.checkIfTooMuch = function () {
+
+    var itemList = $scope.menu.split(",");
+    var itemCount = itemList.length;
+
+    if ($scope.menu == "") {
+      $scope.message = "Please enter data first"
+    } else if (itemCount < 4) {
+      $scope.message = "Enjoy!"
+    } else {
+      $scope.message = "Too much!"
+    }
+
+    console.log($scope.message);
+  }
+
+}
+
+
+
+})();
